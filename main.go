b7925e59e435bf63e6959f51ec4323eaf1b73da1
Jupp Müller <jupp0r@gmail.com>
2016-01-30 16:10:37 +0200

    Added topics, hub

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
+
+var hub = NewHub()
+
+type connection interface{}
 
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -34,9 +39,36 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func subscribe(w http.ResponseWriter, r *http.Request) {
+	// vars := mux.Vars(r)
+	// topic := vars["topic"]
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+
+}
+
+func publish(w http.ResponseWriter, r *http.Request) {
+	// vars := mux.Vars(r)
+	// topic := vars["topic"]
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+
+}
+
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
-	http.HandleFunc("/echo", echo)
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/echo", echo)
+	r.HandleFunc("/subscribe/{topic}", subscribe)
+	r.HandleFunc("/publish/{topic}", publish)
+	log.Fatal(http.ListenAndServe(*addr, r))
 }
